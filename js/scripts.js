61913932237c34074d9d292ae44c3be05b750c1a
Magdalena Szewczyk <mjszewczyk91@gmail.com>
2018-02-22 21:22:09 +0100

    Add index.html and scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// zadanie - instrukcje warunkowe
+var a = prompt('Podaj wartość a');
+var b = prompt('Podaj wartość b');
+var value = (a * a) - (2 * a * b) - (b * b);
+
+var msg = 'wynik obliczeń ' + value;
+alert(msg); 
+console.log(msg);
+
+var msg2;
+if (value > 0)
+{
+    msg2 = 'wynik dodatni';
+}
+else if (value < 0)
+{
+    msg2 = 'wynik ujemny';
+}
+else 
+{
+    msg2 = 'wynik zerowy';
+}
+alert(msg2);
+console.log(msg2);
